2831dd20219aee9fa18272f6ce4c1316e5beef36
strupello <irek1992@gmail.com>
2025-11-07 00:08:50 +0100

    pratice lesson one with ChatGPT

--- main.js
+++ main.js
@@ -2,22 +2,36 @@
 console.log("🔥 Siema Byczku, JavaScript działa! 🔥");
 
 // 2️⃣ Zmienne - czyli pudełka na dane
-let imie = "Byczku";
-const wiek = 33;
+let imie = "Tomek";
+let miasto = "Nowy Sącz";
+const wiek = 55;
+const czas = "rok";
+
 
 console.log(imie);
 console.log(wiek);
+console.log(miasto);
+console.log(czas);
 
 // 3️⃣ Łączenie tekstu
-console.log("Mam na imię " + imie + " i mam " + wiek + " lata.");
+console.log("Mam na imię " + imie + " i mam " + wiek + " lata, pochodze z " + miasto + " miasta. Chce zmienić prace za " + czas + " a ty mi masz w tym pomóc!");
 
 // 4️⃣ Operacje matematyczne
-let a = 5;
-let b = 3;
+let a = 20;
+let b = 5;
+let c = 16;
+let d = 4;
 let suma = a + b;
+
 
 console.log("Suma a + b wynosi: " + suma);
 
 // 5️⃣ Inne przykłady
 let iloczyn = a * b;
 console.log("Iloczyn a * b = " + iloczyn);
+
+let iloraz = a / b;
+console.log("Iloraz a / b = " + iloraz);
+
+let różnica = a - c;
+console.log("różnica a - b = " + różnica);
